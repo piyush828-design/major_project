194cb590206608c19141ff408a26690bd9e098c6
piyush828-design <68499930+piyush828-design@users.noreply.github.com>
2023-02-01 16:06:01 +0530

    updated settings.py

--- dj24/settings.py
+++ dj24/settings.py
@@ -129,7 +129,7 @@ LOGIN_URL= "handleLogin"
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'piyushpawar171@gmail.com'
-EMAIL_HOST_PASSWORD = 'idviemlwefhzcryg'
+EMAIL_HOST_USER = 'your mail id'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
